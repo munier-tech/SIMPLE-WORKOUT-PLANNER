d6966075660569ac3798d945390def77a0def710
munier-tech <muniry399@gmail.com>
2025-07-12 19:36:34 +0300

    ,

--- backend/index.js
+++ backend/index.js
@@ -27,10 +27,10 @@ app.use("/api/workoutLog", workoutLog);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "FRONTEND1", "dist")))
+  app.use(express.static(path.join(__dirname, "frontend", "dist")))
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "FRONTEND1", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
